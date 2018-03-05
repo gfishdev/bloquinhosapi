1caa7460269f08af56a77da0996ec3843e750a97
guilhermeandrade <guilhermeandrade.ti@gmail.com>
2018-03-05 20:47:07 -0300

    User Controller

---
+++ src/main/java/br/com/guilherme/fiap/bloquinhosapi/controller/UserController.java
@@ -0,0 +1,25 @@
+package br.com.guilherme.fiap.bloquinhosapi.controller;
+
+import br.com.guilherme.fiap.bloquinhosapi.component.UserComponent;
+import br.com.guilherme.fiap.bloquinhosapi.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping(value = "user")
+public class UserController {
+
+    @Autowired
+    public UserComponent userComponent;
+
+    @GetMapping("/username/{name}/{password}")
+    public User findUser(@PathVariable(value = "name") String name, @PathVariable(value = "password") String password) {
+        return userComponent.findUser(name, password);
+    }
+
+    @PostMapping()
+    public User save(@RequestBody User user) {
+        userComponent.save(user);
+        return user;
+    }
+}
