17b19c9f7ffa97e34b3eba01c4d1b9da2e1e070b
guilhermeandrade <guilhermeandrade.ti@gmail.com>
2018-03-05 21:41:46 -0300

    User Controller

--- src/main/java/br/com/guilherme/fiap/bloquinhosapi/component/UserComponent.java
+++ src/main/java/br/com/guilherme/fiap/bloquinhosapi/component/UserComponent.java
@@ -14,7 +14,7 @@ public class UserComponent {
     UserRepository userRepository;
 
     public void save(User user) {
-        List<User> users = userRepository.findByName(user.getUsername(), user.getPassword());
+        List<User> users = userRepository.findByUsername(user.getUsername(), user.getPassword());
         if (!users.isEmpty()) {
             user.setId(users.get(0).getId());
         }
@@ -22,9 +22,9 @@ public class UserComponent {
     }
 
     // TODO: Encontrar a maneira mais segura de fazer o login e retornar o usuario sem senha
-    public User findUser(String name, String password) {
+    public User findUser(String username, String password) {
         User user;
-        List<User> users = userRepository.findByName(name, password);
+        List<User> users = userRepository.findByUsername(username, password);
         if (users.isEmpty())
             return new User();
         else {
@@ -33,9 +33,9 @@ public class UserComponent {
         }
     }
 
-    public void inactive(String name, String password) {
+    public void inactive(String username, String password) {
         User user;
-        List<User> users = userRepository.findByName(name, password);
+        List<User> users = userRepository.findByUsername(username, password);
         if (!users.isEmpty()) {
             user = users.get(0);
             user.setActive(false);
